1345f0b9b6a7e5f7d44963fa774e6fab827aadcd
gv4383 <gregory.vargas18@gmail.com>
2022-08-13 22:31:11 -0500

    Add category and description text to device VC

--- Tables/DeviceDetailsViewController.swift
+++ Tables/DeviceDetailsViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 class DeviceDetailsViewController: UIViewController {
     var device: Device!
     
+    let categoryLabel = UILabel()
+    let descriptionLabel = UILabel()
+    let descriptionContainerView = UIView()
+    
     init(device: Device) {
         super.init(nibName: nil, bundle: nil)
         self.device = device
@@ -21,13 +25,56 @@ class DeviceDetailsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configure()
-        print("DEVICE: \(device.name)")
+        
+        configureVC()
+        configureLabels()
+        configureContainerView()
+        layoutUI()
     }
     
-    private func configure() {
+    private func configureVC() {
         view.backgroundColor = .systemBackground
-        title = device.name
         navigationController?.navigationBar.prefersLargeTitles = false
+        title = device.name
+        categoryLabel.text = "Category - \(device.category)"
+        descriptionLabel.text = device.description
+    }
+    
+    private func configureLabels() {
+        categoryLabel.backgroundColor = .systemBackground
+        descriptionLabel.backgroundColor = .systemBackground
+        descriptionLabel.sizeToFit()
+        descriptionLabel.numberOfLines = 0
+    }
+    
+    private func configureContainerView() {
+        descriptionContainerView.backgroundColor = .systemBackground
+    }
+    
+    private func layoutUI() {
+        view.addSubview(categoryLabel)
+        view.addSubview(descriptionContainerView)
+        descriptionContainerView.addSubview(descriptionLabel)
+        categoryLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionContainerView.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let padding: CGFloat = 12
+        
+        NSLayoutConstraint.activate([
+            categoryLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: padding),
+            categoryLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            categoryLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            categoryLabel.heightAnchor.constraint(equalToConstant: 40),
+            
+            descriptionContainerView.topAnchor.constraint(equalTo: categoryLabel.bottomAnchor, constant: padding),
+            descriptionContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            descriptionContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            descriptionContainerView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -padding),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: descriptionContainerView.topAnchor),
+            descriptionLabel.leadingAnchor.constraint(equalTo: descriptionContainerView.leadingAnchor),
+            descriptionLabel.trailingAnchor.constraint(equalTo: descriptionContainerView.trailingAnchor)
+        ])
     }
 }
